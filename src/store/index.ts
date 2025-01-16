a8f3bf2ac3e9f209996e67f17983fc12f98ef335
Fariz Rifqi <farizrifqi26@gmail.com>
2025-01-17 01:01:50 +0800

    disable warn for logs Map() (non serializeable)

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,9 @@ import settingReducer from './settingSlice'
 import connectionReducer from './connectionSlice'
 import preferencesReducer from './preferencesSlice'
 import liveInfoReducer from './liveInfoSlice'
+import { enableMapSet } from 'immer'
+
+enableMapSet()
 export const store = configureStore({
   reducer: {
     logs: logsReducer,
@@ -14,7 +17,11 @@ export const store = configureStore({
     liveInfo: liveInfoReducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(websocketMiddleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredPaths: ['logs'],
+      },
+    }).concat(websocketMiddleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
