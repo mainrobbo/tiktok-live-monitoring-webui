e8a505877b2eb6c24477c9d5049215a38dc2d142
Fariz Rifqi <farizrifqi26@gmail.com>
2025-01-17 08:22:14 +0800

    add mobile support

--- src/components/app-navbar.tsx
+++ src/components/app-navbar.tsx
@@ -1,8 +1,10 @@
 'use client'
-import { WifiIcon } from 'lucide-react'
+import { ArrowRight, LayoutDashboardIcon, WifiIcon } from 'lucide-react'
 import Link from 'next/link'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/store'
+import { Button } from './ui/button'
+import { useSidebar } from './ui/sidebar'
 
 export default function AppNavbar() {
   const { live, connected } = useSelector(
@@ -11,10 +13,19 @@ export default function AppNavbar() {
   const { username } = useSelector(
     ({ setting }: { setting: RootState['setting'] }) => setting,
   )
-
+  const { toggleSidebar } = useSidebar()
   return (
     <div className='sticky top-0 bg-background border-b p-2 z-50'>
       <div className='flex items-center justify-between'>
+        <div>
+          <Button
+            size={'icon'}
+            onClick={toggleSidebar}
+            className='lg:hidden flex'
+          >
+            <LayoutDashboardIcon />
+          </Button>
+        </div>
         <div className='flex items-center gap-2'>
           {live ? (
             <>
