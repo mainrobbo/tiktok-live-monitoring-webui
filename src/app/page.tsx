2ec8e849102ab505932588ef042150b25bbc95bb
Fariz Rifqi <farizrifqi26@gmail.com>
2025-01-17 08:22:59 +0800

    add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
+'use client'
 import ExpandableChart from '@/components/views/expandable-chart'
+import LandingPage from '@/components/views/landing'
 import ChatList from '@/components/views/list/chat-list'
 import GiftList from '@/components/views/list/gift-list'
 import LikeList from '@/components/views/list/like-list'
+import ShareList from '@/components/views/list/share-list'
+import SocialList from '@/components/views/list/social-list'
 import ViewList from '@/components/views/list/view-list'
 import RoomInfoComponent from '@/components/views/room-info'
+import { useSelector } from 'react-redux'
 
 export default function Home() {
+  const { live, connected } = useSelector((state: any) => state.connection)
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-3 gap-2 w-full justify-items-stretch justify-align-start h-full p-2'>
-      {/* <RoomInfoComponent />*/}
-
-      <div className='w-full lg:col-span-3'>
-        <ExpandableChart />
-      </div>
-      <ChatList />
-      <LikeList />
-      <ViewList />
-      <GiftList />
+    <div className='grid grid-cols-1 lg:grid-cols-3 gap-2 w-full justify-items-stretch justify-align-start h-fit p-2'>
+      {live && connected ? (
+        <>
+          <RoomInfoComponent />
+          <div className='w-full lg:col-span-3'>
+            <ExpandableChart />
+          </div>
+          <ChatList />
+          <LikeList />
+          <GiftList />
+          <ViewList defaultOpen={false} />
+          <ShareList defaultOpen={false} />
+          <SocialList defaultOpen={false} />
+        </>
+      ) : (
+        <LandingPage />
+      )}
     </div>
   )
 }
---
+++ src/components/views/landing.tsx
@@ -0,0 +1,15 @@
+export default function LandingPage() {
+  return (
+    <div className='flex flex-col w-full items-center col-span-3 h-full mt-20'>
+      <div className='flex flex-col gap-5'>
+        <div className='flex flex-col text-4xl font-extrabold tracking-tight lg:text-5xl'>
+          Go Live, Go Viral
+        </div>
+        <div className='tracking-wide font-extralight leading-7 [&:not(:first-child)]:mt-2'>
+          Get ready to disrupt TikTok with ultra-tech live monitoring &
+          streaming suite
+        </div>
+      </div>
+    </div>
+  )
+}
